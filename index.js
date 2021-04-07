eee48b64fb42ed63df29bf7559f80776e84bed6f
jim <jbmbroomfield@gmail.com>
2021-04-07 13:42:58 +1000

    Done

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string,bannedWords=[]) {
+    if (bannedWords.includes(string)) {
+      return string
+    } else {
+      return string[0].toUpperCase() + string.slice(1)
+    }
+  }
+  static sanitize(string) {
+    let result = ''
+    for (const char of string) {
+      if ("qwertyuiopasdfghjklzxcvbnm-' ".includes(char.toLowerCase())) {
+        result += char
+      }
+    }
+    return result
+  }
+  static titleize(string) {
+    string = this.capitalize(string)
+    const array = string.split(' ')
+    const capArray = array.map(word => {
+      return this.capitalize(word,['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'])
+    })
+    return capArray.join(' ')
+  }
 }
